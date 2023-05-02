c84dec136fd67332632a718311c819d8afcfd043
maurinepic <maurinepic@gmail.com>
2023-05-02 14:05:19 +0200

    fix thème enfant

--- wp-content/themes/neve-child/functions.php
+++ wp-content/themes/neve-child/functions.php
@@ -1,14 +1,10 @@
 <?php
-// Exit if accessed directly
-if ( !defined( 'ABSPATH' ) ) exit;
 
-// BEGIN ENQUEUE PARENT ACTION
-// AUTO GENERATED - Do not modify or remove comment markers above or below:
-
-    function neve-child-css() {
-        wp_enqueue_style( 'neve-child-style', get_stylesheet_uri(), array( 'neve-style' ), filemtime(get_stylesheet_directory()) . '/style.css');
-    }
-
-add_action( 'wp_enqueue_scripts', 'neve-child-css' );
+// Action qui permet de charger des scripts dans notre thème
+add_action('wp_enqueue_scripts', 'nevechildcss');
+function nevechildcss(){
+    // Chargement du style.css du thème parent Twenty Twenty
+    wp_enqueue_style('neve-child-style', get_template_directory_uri() . '/style.css');
+}
 
 // END ENQUEUE PARENT ACTION
